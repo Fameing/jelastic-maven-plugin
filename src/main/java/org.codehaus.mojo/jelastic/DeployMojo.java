bf7324e1453a715738764330ad13875cebe533de
igor.yova@gmail.com <igor.yova@gmail.com@6ee92537-495a-5b92-7c93-c91e828673fa>
2011-10-18 20:05:49 +0000

    fix package name

---
+++ src/main/java/org.codehaus.mojo/jelastic/DeployMojo.java
@@ -0,0 +1,66 @@
+package org.codehaus.mojo.jelastic;
+
+import org.codehaus.mojo.jelastic.model.Authentication;
+import org.codehaus.mojo.jelastic.model.CreateObject;
+import org.codehaus.mojo.jelastic.model.Deploy;
+import org.codehaus.mojo.jelastic.model.UpLoader;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * User: Igor.Yova@gmail.com
+ * Date: 6/8/11
+ * Time: 10:30 AM
+ */
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal deploy
+ * @phase install
+ */
+public class DeployMojo extends JelasticMojo {
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        Authentication authentication = authentication();
+        if (authentication.getResult() == 0) {
+            getLog().info("------------------------------------------------------------------------");
+            getLog().info("   Authentication : SUCCESS");
+            getLog().info("          Session : " + authentication.getSession());
+            //getLog().info("              Uid : " + authentication.getUid());
+            getLog().info("------------------------------------------------------------------------");
+            UpLoader upLoader = upload(authentication);
+            if (upLoader.getResult() == 0) {
+                getLog().info("      File UpLoad : SUCCESS");
+                getLog().info("         File URL : " + upLoader.getFile());
+                getLog().info("        File size : " + upLoader.getSize());
+                getLog().info("------------------------------------------------------------------------");
+                CreateObject createObject = createObject(upLoader,authentication);
+                if (createObject.getResult() == 0) {
+                getLog().info("File registration : SUCCESS");
+                getLog().info("  Registration ID : " + createObject.getResponse().getObject().getId());
+                getLog().info("     Developer ID : " + createObject.getResponse().getObject().getDeveloper());
+                getLog().info("------------------------------------------------------------------------");
+                    Deploy deploy = deploy(authentication,upLoader,createObject);
+                    if (deploy.getResponse().getResult() == 0) {
+                        getLog().info("      Deploy file : SUCCESS");
+                        getLog().info("       Deploy log :");
+                        getLog().info(deploy.getResponse().getResponses()[0].getOut());
+                    } else {
+                        getLog().error("          Deploy : FAILED");
+                        getLog().error("           Error : " + deploy.getResponse().getError());
+                    }
+                }
+            } else {
+                getLog().error("File upload : FAILED");
+                getLog().error("      Error : " + upLoader.getError());
+            }
+        } else {
+            getLog().error("Authentication : FAILED");
+            getLog().error("         Error : " + authentication.getError());
+        }
+
+
+    }
+
+}
---
+++ src/main/java/org.codehaus.mojo/jelastic/model/LogsResponse.java
@@ -0,0 +1,55 @@
+package org.codehaus.mojo.jelastic.model;
+
+/**
+ * User: Igor.Yova@gmail.com
+ * Date: 6/9/11
+ * Time: 7:16 PM
+ */
+public class LogsResponse {
+    private String body;
+    private int result;
+    private int linescount;
+    private String error;
+
+    public LogsResponse() {
+    }
+
+    public LogsResponse(String body, int result, int linescount, String error) {
+        this.body = body;
+        this.result = result;
+        this.linescount = linescount;
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public int getLinescount() {
+        return linescount;
+    }
+
+    public void setLinescount(int linescount) {
+        this.linescount = linescount;
+    }
+}
