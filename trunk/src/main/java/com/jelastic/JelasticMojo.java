68a7405ff2e677ef3aa6a377d7b11c23c1adb1fd
igor.yova@gmail.com <igor.yova@gmail.com@6ee92537-495a-5b92-7c93-c91e828673fa>
2012-01-23 11:31:33 +0000

    change protocol to https and SSLContext create

--- trunk/src/main/java/com/jelastic/JelasticMojo.java
+++ trunk/src/main/java/com/jelastic/JelasticMojo.java
@@ -19,13 +19,18 @@ import org.apache.http.HttpHost;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIUtils;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.FileBody;
@@ -41,11 +46,16 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.settings.Proxy;        
 import org.codehaus.jackson.map.ObjectMapper;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -281,6 +291,7 @@ public abstract class JelasticMojo extends AbstractMojo {
             }
             try {
                 DefaultHttpClient httpclient = new DefaultHttpClient();
+                httpclient = wrapClient(httpclient);
                 if (http_proxy != null) {
                     httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, http_proxy);
                 }
@@ -320,6 +331,7 @@ public abstract class JelasticMojo extends AbstractMojo {
         }
         try {
             DefaultHttpClient httpclient = new DefaultHttpClient();
+            httpclient = wrapClient(httpclient);
             if (http_proxy != null) {
                 httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, http_proxy);
             }
@@ -369,6 +381,7 @@ public abstract class JelasticMojo extends AbstractMojo {
         }
         try {
             DefaultHttpClient httpclient = new DefaultHttpClient();
+            httpclient = wrapClient(httpclient);
             if (http_proxy != null) {
                 httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, http_proxy);
             }
@@ -420,9 +433,11 @@ public abstract class JelasticMojo extends AbstractMojo {
         }
         try {
             DefaultHttpClient httpclient = new DefaultHttpClient();
+            httpclient = wrapClient(httpclient);
             if (http_proxy != null) {
                 httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, http_proxy);
             }
+
             httpclient.setCookieStore(getCookieStore());
             List<NameValuePair> qparams = new ArrayList<NameValuePair>();
             qparams.add(new BasicNameValuePair("charset", "UTF-8"));
@@ -458,5 +473,30 @@ public abstract class JelasticMojo extends AbstractMojo {
                 message.addHeader(key, value);
             }
         }    
+    }
+
+    public static DefaultHttpClient wrapClient(DefaultHttpClient base) {
+        try {
+            SSLContext ctx = SSLContext.getInstance("TLS");
+            X509TrustManager tm = new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+                }
+                public void checkServerTrusted(X509Certificate[] xcs, String string) throws CertificateException {
+                }
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+            ctx.init(null, new TrustManager[]{tm}, null);
+            SSLSocketFactory ssf = new SSLSocketFactory(ctx);
+            ssf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            ClientConnectionManager ccm = base.getConnectionManager();
+            SchemeRegistry sr = ccm.getSchemeRegistry();
+            sr.register(new Scheme("https", ssf, 443));
+            return new DefaultHttpClient(ccm, base.getParams());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 }
